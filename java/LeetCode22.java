faaffa644a0cc4ad93eba9f67fb30a0ed76b1496
Luis Moreno <luismoreno2103@gmail.com>
2023-10-30 10:48:30 -0500

    shell of recursive method, needs changes

--- java/LeetCode22.java
+++ java/LeetCode22.java
@@ -6,6 +6,33 @@
  * Output: ["((()))","(()())","(())()","()(())","()()()"]
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeetCode22 {
-    
+    List<String> ans = new ArrayList<>();
+    public List<String> generateParenthesis(int n) {
+        helper("(", n);
+        return ans;
+    }
+
+    public void helper(String s, int n) {
+        // Base case
+        if (n == 0) {
+            ans.add(s);
+            return;
+        }
+
+        char c = s.charAt(s.length() - 1);
+
+        if (c == '(') {
+            s.concat(")");
+            helper(s, n - 1);
+        }
+
+        if (c == ')') {
+            
+        }
+
+    }
 }
