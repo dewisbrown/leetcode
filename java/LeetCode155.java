1189461ad87c45acd408e6912f2480baf07c34c4
Luis Moreno <luismoreno2103@gmail.com>
2023-10-30 08:47:48 -0500

    used linked list solution instead of arrays as stacks

--- java/LeetCode155.java
+++ java/LeetCode155.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Design a stack that supports push, pop, top, 
  * and retrieving the minimum element in constant time.
 
@@ -11,69 +11,42 @@
  * int getMin() retrieves the minimum element in the stack.
  * 
  * You must implement a solution with O(1) time complexity for each function.
+ * 
+ * LeetCode155 -> MinStack class
  */
 
- // LeetCode155 -> MinStack class
 public class LeetCode155 {
-    private final int SIZE = 20;
-    private int[] stack;
-    private int top;
-    private int min;
+    private Node head;
 
-    public LeetCode155() {
-        stack = new int[SIZE];
-        top = -1;
-        min = Integer.MIN_VALUE;
-    }
-
-    /*
-     * When inserting a new minimum element, a different 
-     * value is stored in the stack than the supplied value:
-     * 1. Insert calulated number into stack
-     *      stack[++top] = 2 * j - min
-     * 2. update min:
-     *      min = j;
-     */
-    public void push(int j) {
-        if (isEmpty()) {
-            min = j;
-            stack[++top] = j;
+    public void push(int val) {
+        if (head == null) {
+            head = new Node(val, val, null);
         } else {
-            if (j >= min) {
-                stack[++top] = j;
-            } else {
-                stack[++top] = 2 * j - min;
-                min = j;
-            }
+            head = new Node(val, Math.min(val, head.min), head);
         }
     }
     
-    /* A similar process is done when removing the 
-     * minimum element:
-     * 1. use a temp variable to hold min,
-     * 2. update min:
-     *      min = 2 * min - stack[top--];
-     * 3. return temp variable
-     */
-    public int pop() {
-        if (top() < min) {          // if top is less than minimum
-            int temp = min;         // return min
-            min = 2 * min - stack[top--];// update min value
-            return temp;
-        } else {
-            return stack[top--];
-        }
+    public void pop() {
+        head = head.next;
     }
 
     public int top() {
-        return stack[top];
+        return head.val;
     }
 
     public int getMin() {
-        return min;
+        return head.min;
     }
+}
 
-    public boolean isEmpty() {
-        return top == -1;
+class Node {
+    int val;
+    int min;
+    Node next;
+
+    public Node(int val, int min, Node next) {
+        this.val = val;
+        this.min = min;
+        this.next = next;
     }
 }
