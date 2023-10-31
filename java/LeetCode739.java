edf5459e6bd72745581010b9f127efc994fdc796
dewisbrown <luismoreno2103@gmail.com>
2023-10-31 12:50:06 -0500

    accepted solution but terrible runtime

--- java/LeetCode739.java
+++ java/LeetCode739.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given an array of integers temperatures represents 
  * the daily temperatures, return an array answer such 
@@ -12,6 +15,21 @@ public class LeetCode739 {
     public int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
         int[] ans = new int[n];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(temperatures[i]) && i < map.get(temperatures[i])) {
+                ans[i] = map.get(temperatures[i]) - i;
+                continue;
+            }
+            for (int j = i + 1; j < n; j++) {
+                if (temperatures[i] < temperatures[j]) {
+                    map.put(temperatures[i], j);
+                    ans[i] = j - i;
+                    break;
+                }
+            }
+        }
         
         return ans;
     }
