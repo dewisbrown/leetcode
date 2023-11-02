7f7b836fccf18c0b6bef3c4d83d53a93a920608f
dewisbrown <luismoreno2103@gmail.com>
2023-11-02 09:49:09 -0500

    stack solution using helper class Pair

--- java/LeetCode853.java
+++ java/LeetCode853.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * There are n cars going to the same destination along a one-lane road. 
  * The destination is target miles away.
@@ -18,7 +24,70 @@
  */
 public class LeetCode853 {
     public int carFleet(int target, int[] position, int[] speed) {
+        Stack<Double> stack = new Stack<>();
+        int n = position.length;
+        
+        // Create pairs
+        List<Pair> pairs = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            pairs.add(new Pair(position[i], speed[i]));
+        }
 
-        return 0;
+        // Sort pairs
+        pairs.sort(Comparator.comparing(Pair::getA));
+
+        // Loop through sorted pair list
+        for (int i = n - 1; i >= 0; i--) {
+            double oldTop = -1;
+            if (!stack.isEmpty()) {
+                oldTop = stack.peek();
+            }
+
+            stack.push((double)(target - pairs.get(i).a) / pairs.get(i).b);
+            
+            // compare top of stack with 2nd stack element
+            if (stack.size() >= 2 && stack.peek() <= oldTop) {
+                stack.pop();
+            }
+        }
+        return stack.size();
     }
 }
+
+class Pair {
+    int a;
+    int b;
+
+    public Pair(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", a, b);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pair pair = (Pair) o;
+        return pair.a == a && pair.b == b;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(a) * 31 + Integer.hashCode(b);
+    }
+}
+
