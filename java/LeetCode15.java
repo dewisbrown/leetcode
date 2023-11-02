5729a7e0c5bd9ca758503dbc95c5aebc2a7146f6
dewisbrown <luismoreno2103@gmail.com>
2023-11-02 14:15:04 -0500

    shell of solution

---
+++ java/LeetCode15.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given an integer array nums, return all the triplets 
+ * [nums[i], nums[j], nums[k]] such that 
+ * i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+ * 
+ * Notice that the solution set must not contain duplicate triplets.
+ */
+public class LeetCode15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        
+
+        return res;
+    }
+}
