25642c5e5c4f171a88d73e7cd79d851c22e479a3
dewisbrown <luismoreno2103@gmail.com>
2023-11-03 16:01:19 -0500

    two pointer solution

--- java/LeetCode15.java
+++ java/LeetCode15.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 /**
  * Given an integer array nums, return all the triplets 
@@ -11,8 +12,37 @@ import java.util.List;
 public class LeetCode15 {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums);
 
-        
+        for (int i = 0; i < n; i++) {
+            // Handles duplicate values for 'a' (a + b + c = 0)
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            // Starting position of pointers
+            int l = i + 1;
+            int r = n - 1;
+            
+            while (l < r) {
+                int threeSum = nums[i] + nums[l] + nums[r];
+                if (threeSum > 0) {
+                    r--;
+                } else if (threeSum < 0) {
+                    l++;
+                } else {
+                    List<Integer> list = new ArrayList<>(Arrays.asList(nums[i], nums[l], nums[r]));
+                    res.add(list);
+
+                    // update left pointer after adding threeSum list
+                    l++;
+                    while (nums[l] == nums[l - 1] && l < r) {
+                        l++;
+                    }
+                }
+            }
+        }
 
         return res;
     }
