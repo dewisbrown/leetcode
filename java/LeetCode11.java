82eafe5d4cb4b47d78cd4a7d633ce2dfbe0cc415
Luis Moreno <luismoreno2103@gmail.com>
2023-11-07 10:51:37 -0600

    two pointer solution

--- java/LeetCode11.java
+++ java/LeetCode11.java
@@ -11,8 +11,29 @@
  */
 public class LeetCode11 {
     public int maxArea(int[] height) {
+        int n = height.length;
         int maxArea = 0;
-
+        int l = 0;
+        int r = n - 1;
+        
+        /* 
+         * left pointer at index 0, right pointer at n - 1
+         * 1. find area
+         * 2. compare to maxArea and update if larger
+         * 3a. increment left pointer if height[l] < height[r]
+         * 3b. decrement right pointer if height[r] < height[l]
+         */ 
+ 
+        while (l < r) {
+            int area = Math.min(height[l], height[r]) * (r - l);
+            maxArea = Math.max(area, maxArea);
+            
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
         return maxArea;
     }
 }
