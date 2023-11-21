5171c5939c53d480eb845b9374a124943452ff33
Luis Moreno <luismoreno2103@gmail.com>
2023-11-21 09:11:39 -0600

    correct solution and test

--- java/LeetCode143.java
+++ java/LeetCode143.java
@@ -10,7 +10,55 @@
  * Only nodes themselves may be changed.
  */
 public class LeetCode143 {
+    public LeetCode143() {}
     public void reorderList(ListNode head) {
-        
+        ListNode fast = head.next;
+        ListNode slow = head;
+
+        // use fast and slow pointer to find middle of linked list
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        ListNode second = slow.next;    // starting node of second half of list
+        slow.next = null;               // remove link to second half
+
+        ListNode prev = null;
+        while (second != null) {        // reverse second half
+            ListNode temp = second.next;
+            second.next = prev;
+            prev = second;
+            second = temp;
+        }
+        second = prev;
+
+        ListNode first = head;
+        while (second != null) {        // merge two halves
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+
+            first.next = second;
+            second.next = temp1;
+
+            // move pointers
+            second = temp2;
+            first = temp1;
+        }
+    }
+
+    public void reverseList(ListNode node) {
+        ListNode prev = new ListNode();
+        ListNode curr = node;
+        ListNode next = new ListNode();
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        node = prev;
     }
 }
--- java/Tester.java
+++ java/Tester.java
@@ -4,6 +4,32 @@
 public class Tester {
     
     public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
         
+        LeetCode143 test = new LeetCode143();
+
+        ListNode curr = head;
+
+        System.out.println("Before reorder:");
+        while (curr != null) {
+            System.out.print("[" + curr.val + "] -> ");
+            curr = curr.next;
+        }
+        System.out.println();
+
+        test.reorderList(head);
+
+        curr = head;
+
+        System.out.println("After reorder:");
+        while (curr != null) {
+            System.out.print("[" + curr.val + "] -> ");
+            curr = curr.next;
+        }
+        System.out.println();
     }
 }
