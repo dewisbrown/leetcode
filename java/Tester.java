e8862b0407e63f4c29b463032ed9e335938fb58e
Luis Moreno <luismoreno2103@gmail.com>
2023-11-21 09:57:37 -0600

    creates linked list

--- java/Tester.java
+++ java/Tester.java
@@ -5,31 +5,16 @@ public class Tester {
     
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
-        
-        LeetCode143 test = new LeetCode143();
-
         ListNode curr = head;
 
-        System.out.println("Before reorder:");
-        while (curr != null) {
-            System.out.print("[" + curr.val + "] -> ");
+        for (int i = 2; i < 6; i++) {
+            curr.next = new ListNode(i);
             curr = curr.next;
         }
-        System.out.println();
 
-        test.reorderList(head);
-
-        curr = head;
-
-        System.out.println("After reorder:");
-        while (curr != null) {
-            System.out.print("[" + curr.val + "] -> ");
-            curr = curr.next;
+        while (head != null) {
+            System.out.printf("[%d] -> ", head.val);
+            head = head.next;
         }
-        System.out.println();
     }
 }
