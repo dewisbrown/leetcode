d7702eb296a03045bc7573b6ca96fc2a4ab2f5a3
dewisbrown <luismoreno2103@gmail.com>
2023-11-26 19:08:03 -0600

    empty main method

--- java/Tester.java
+++ java/Tester.java
@@ -4,17 +4,6 @@
 public class Tester {
     
     public static void main(String[] args) {
-        ListNode head = new ListNode(1);
-        ListNode curr = head;
-
-        for (int i = 2; i < 6; i++) {
-            curr.next = new ListNode(i);
-            curr = curr.next;
-        }
-
-        while (head != null) {
-            System.out.printf("[%d] -> ", head.val);
-            head = head.next;
-        }
+        
     }
 }
