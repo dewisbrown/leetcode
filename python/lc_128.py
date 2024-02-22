da9ac54542282ca062d366eeecd13c4259c8e6bc
Luis Moreno <luismoreno2103@gmail.com>
2024-02-22 08:14:52 -0600

    checks for start of sequence and counts lengths

---
+++ python/lc_128.py
@@ -0,0 +1,31 @@
+"""
+128. Longest Consecutive Sequence
+
+Given an unsorted array of integers nums,
+return the length of the longest consecutive
+elements in sequence.
+
+You must write an algorithm that runs in O(n) time.
+"""
+def longestConsecutive(nums: list[int]) -> int:
+    nums_set = set(nums)
+    longest = 0
+
+    for n in nums:
+        # check if n is the start of a sequence
+        if (n - 1) not in nums_set:
+            length = 0
+            # count sequence of numbers from there
+            while n + length in nums_set:
+                length += 1
+            longest = max(length, longest)
+
+    return longest
+
+ex_in = [
+    [0,3,7,2,5,8,4,6,0,1],
+    [100,4,200,1,3,2]
+]
+
+for ex in ex_in:
+    print(longestConsecutive(ex))
