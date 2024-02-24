b54d652d44a862091dcbee11647d450be98f653a
Luis Moreno <luismoreno2103@gmail.com>
2024-02-23 18:48:11 -0600

    shell

---
+++ python/lc_22.py
@@ -0,0 +1,20 @@
+"""
+22. Generate Parentheses
+
+Given n pairs of parentheses, write a function to 
+generate all combinations of well-formed parentheses.
+"""
+def generateParenthesis(n: int) -> list[str]:
+    """
+    Use backtracking.
+    """
+    ans = []
+
+    
+
+    return ans
+
+
+ex_in = [3, 1]
+for ex in ex_in:
+    print(generateParenthesis(ex))
