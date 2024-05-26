1aee07a357f0587b0cb7ad21a79a1cc11e84c56b
dewisbrown <luismoreno2103@gmail.com>
2024-05-26 11:15:10 -0500

    another one

--- python/lc_605.py
+++ python/lc_605.py
@@ -8,9 +8,40 @@ return true if n new flowers can be planted in the flowerbed
 without violating the no-adjacent-flowers rule and false otherwise.
 """
 def canPlaceFlowers(flowerbed: list[int], n: int) -> bool:
+    # Edge cases
+    if n == 0:
+        return True
+    if len(flowerbed) == 0:
+        return False
+    if len(flowerbed) == 1:
+        return flowerbed[0] == 0
+
+    # Not optimal solution, but works
+    m = len(flowerbed)
+    l = 0
+    r = 0
     count = 0
-    
-    return n <= count
+    while l < m:
+        if l == r:
+            r += 1
+            continue
+        # Check right neighbor of 1st element
+        if l == 0:
+            if flowerbed[l] == 0 and flowerbed[r] == 0:
+                count += 1
+                flowerbed[l] = 1
+        # Check left neighbor of last element
+        elif l == m - 1:
+            if flowerbed[l] == 0 and flowerbed[l - 1] == 0:
+                count += 1
+                flowerbed[l] = 1
+        # Check left and right neighbor or an element
+        else:
+            if flowerbed[l - 1] == 0 and flowerbed[l] == 0 and flowerbed[r] == 0:
+                count += 1
+                flowerbed[l] = 1
+        l += 1
+    return count >= n
 
 inputs = [
     [[1,0,0,0,1], 1],
