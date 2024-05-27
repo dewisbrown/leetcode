6a8b5767d61f1dd4171f0e69fd3f288a877f0f7d
dewisbrown <luismoreno2103@gmail.com>
2024-05-27 14:35:50 -0500

    keep track of min value

--- python/lc_334.py
+++ python/lc_334.py
@@ -8,16 +8,25 @@ such that i < j < k and nums[i] < nums[j] < nums[k].
 If no such indices exists, return false.
 """
 def increasingTriplet(nums: list[int]) -> bool:
+    first = second = float('inf') 
+    for num in nums: 
+        if num <= first:
+            first = num
+        elif num <= second:
+            second = num
+        else:
+            return True
     return False
 
 ins = [
     [1,2,3,4,5],
     [5,4,3,2,1],
-    [2,1,5,0,4,6]
+    [2,1,5,0,4,6],
+    [20,100,10,12,5,13]
 ]
 
 outs = [
-    True, False, True
+    True, False, True, True
 ]
 
 for i, o in zip(ins, outs):
